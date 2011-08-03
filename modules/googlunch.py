66666d087c0c5ab9f62616ec3c67928288d8f66a
George Hickman <george@ghickman.co.uk>
2011-08-03 16:12:51 +0100

    convert to using a json config file with nicer defaults

--- modules/googlunch.py
+++ modules/googlunch.py
@@ -8,7 +8,17 @@ import os
 import urllib2
 import json
 
-default = 'Cafe Baci'
+from textwrap import dedent as trim
+
+default = """\
+    {
+        'address': '',
+        'api_key': '',
+        'latitude': '',
+        'longtitude': ''
+    }
+    # EOF
+    """
 
 def choose_lunch(phenny, input):
     lat,lng,choice=load_places_from_google()
@@ -20,8 +30,6 @@ choose_lunch.commands = ['googlunch', 'googfood','gl']
 def get_directions(lat,lng):
     big_url='http://maps.google.com/maps?saddr=500+W+Madison+St,+Chicago,+IL+60606&daddr='+lat+','+lng+'&dirflg=w'
     return urllib2.urlopen("http://tinyurl.com/api-create.php?url="+big_url).read()
-
-
 
 def ensure(path):
     if os.path.isfile(path):
@@ -46,20 +54,17 @@ def ensure(path):
         f.close()
     return True
 
-#Having only the Api Key in the Conf File for now
-#To be changed later with more configuration items
-def load_api_key():
-    conf_file = os.path.join(os.path.expanduser('~/.phenny'),'googlunch_config.txt')
-    if ensure(conf_file):
-        return [l.strip() for l in open(conf_file)]
+def get_places_list():
+    url = 'https://maps.googleapis.com/maps/api/place/search/json?location=%s,%s&radius=500&types=food&sensor=false&key=%s'
+    conf = json.loads(os.path.join(os.path.expanduser('~/.phenny'), 'googlunch.json'))
+    if ensure(conf):
+        url % (conf.latitude, conf.longitude, conf.api_key)
+        return urllib2.urlopen(url).read()
     else:
         return None
 
 def load_places_from_google():
-    api_key=load_api_key()
-    url='https://maps.googleapis.com/maps/api/place/search/json?location=41.882199,-87.640493&radius=500&types=food&sensor=false&key='+api_key[0]
-    places = urllib2.urlopen(url).read()
-    jplaces=json.loads(places)
+    jplaces = json.loads(get_places_list())
     list_geo = [result['geometry'] for result in jplaces['results']]
     list_names = [result['name'] for result in jplaces['results']]
     index = int(random.uniform(0,len(list_geo)))
